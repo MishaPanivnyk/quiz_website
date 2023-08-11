d0dba1b12f55352c7573ec3ea2a0eb8154527c1c
Misha Panivnyk <panivnykm@gmail.com>
2023-08-11 20:09:20 +0300

    add constants

--- questions.js
+++ questions.js
@@ -1,57 +1,48 @@
 let questions = [
-    {
-        numb: 1,
-        question: "What does HTML stand for?",
-        answer: "C. Hyper Text Markup Language",
-        options: [
-            "A. Hyper Type Multi Language",
-            "B. Hyper Text Multiple Language",
-            "C. Hyper Text Markup Language",
-            "D. Home Text Multi Language"
-        ]
-    },
-    {
-        numb: 2,
-        question: "What does CSS stand for?",
-        answer: "A. Cascading Style Sheet",
-        options: [
-            "A. Cascading Style Sheet",
-            "B. Cute Style Sheet",
-            "C. Computer Style Sheet",
-            "D. Codehal Style Sheet"
-        ]
-    },
-    {
-        numb: 3,
-        question: "What does PHP stand for?",
-        answer: "A. Hypertext Preprocessor",
-        options: [
-            "A. Hypertext Preprocessor",
-            "B. Hometext Programming",
-            "C. Hypertext Preprogramming",
-            "D. Programming Hypertext Preprocessor"
-        ]
-    },
-    {
-        numb: 4,
-        question: "What does SQL stand for?",
-        answer: "D. Structured Query Language",
-        options: [
-            "A. Strength Query Language",
-            "B. Stylesheet Query Language",
-            "C. Science Question Language",
-            "D. Structured Query Language"
-        ]
-    },
-    {
-        numb: 5,
-        question: "What does XML stand for?",
-        answer: "D. Extensible Markup Language",
-        options: [
-            "A. Excellent Multiple Language",
-            "B. Explore Multiple Language",
-            "C. Extra Markup Language",
-            "D. Extensible Markup Language"
-        ]
-    }
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "C. Hyper Text Markup Language",
+    options: [
+      "A. Hyper Type Multi Language",
+      "B. Hyper Text Multiple Language",
+      "C. Hyper Text Markup Language",
+      "D. Home Text Multi Language",
+    ],
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "A. Cascading Style Sheet",
+    options: [
+      "A. Cascading Style Sheet",
+      "B. Cute Style Sheet",
+      "C. Computer Style Sheet",
+      "D. Codehal Style Sheet",
+    ],
+  },
+  {
+    numb: 3,
+    question: "What command would you use to create a new git repository?",
+    answer: "D. git init",
+    options: ["A. git add", "B. git start", "C. git new", "D. git init"],
+  },
+  {
+    numb: 4,
+    question:
+      "If you want to import just the Component from the React library, what syntax do you use?",
+    answer: "C. import { Component } from 'react'",
+    options: [
+      "A. import React.Component from 'react'",
+      "B. import [ Component ] from 'react'",
+      "C. import { Component } from 'react'",
+      "D. import Component from 'react'",
+    ],
+  },
+  {
+    numb: 5,
+    question: "To create a constant in JavaScript, which keyword do you use?",
+    answer: "A. const",
+    options: ["A. const", "B. let", "C. constant", "D. var"],
+  },
 ];
