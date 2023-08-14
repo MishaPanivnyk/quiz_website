349b4776c1d4c06f6beafb450ba344a89ec61068
Misha Panivnyk <panivnykm@gmail.com>
2023-08-14 13:28:05 +0300

    add 5 question

--- questions.js
+++ questions.js
@@ -45,4 +45,45 @@ let questions = [
     answer: "A. const",
     options: ["A. const", "B. let", "C. constant", "D. var"],
   },
+  {
+    numb: 6,
+    question:
+      "Which attribute must have a unique value each time it is used in an HTML document?",
+    answer: "D. id",
+    options: ["A. title", "B. class", "C. style", "D. id"],
+  },
+  {
+    numb: 7,
+    question: "Which CSS property will not trigger layout recalculation?",
+    answer: "B. opacity",
+    options: ["A. top", "B. opacity", "C. width", "D. height"],
+  },
+  {
+    numb: 8,
+    question: "Which choice is not part of CSS box model?",
+    answer: "D. paragraph",
+    options: ["A. margin", "B. border", "C. padding", "D. paragraph"],
+  },
+  {
+    numb: 9,
+    question: "Which method converts JSON data to a JavaScript object?",
+    answer: "B. JSON.parse()",
+    options: [
+      "A. JSON.fromString()",
+      "B. JSON.parse()",
+      "C. JSON.toObject()",
+      "D. JSON.stringify()",
+    ],
+  },
+  {
+    numb: 10,
+    question: "Which method do you use to attach one DOM node to another?",
+    answer: "D. appendChild()",
+    options: [
+      "A. attachNode()",
+      "B. getNode()",
+      "C. querySelector()",
+      "D. appendChild()",
+    ],
+  },
 ];
